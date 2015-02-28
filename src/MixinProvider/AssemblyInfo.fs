e0062bcb29c849af211ebb6433e0d9813e61b108
pezi <pezi_pink@pinksquirrellabs.com>
2015-02-28 13:00:13 +0000

    scaffold

---
+++ src/MixinProvider/AssemblyInfo.fs
@@ -0,0 +1,12 @@
+﻿namespace System
+open System.Reflection
+
+[<assembly: AssemblyTitleAttribute("MixinProvider")>]
+[<assembly: AssemblyProductAttribute("MixinProvider")>]
+[<assembly: AssemblyDescriptionAttribute("Type provider for generating F# code via compile time metaprograms")>]
+[<assembly: AssemblyVersionAttribute("1.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.0")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "1.0"
