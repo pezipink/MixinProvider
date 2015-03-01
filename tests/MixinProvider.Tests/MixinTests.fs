2389e454b0b90de6378ad828b981afea03c8e62b
pezi <pezi_pink@pinksquirrellabs.com>
2015-03-01 16:05:08 +0000

    comment test that won't work for others as currently using absolute paths

--- tests/MixinProvider.Tests/MixinTests.fs
+++ tests/MixinProvider.Tests/MixinTests.fs
@@ -17,7 +17,7 @@ type Basic_Test = mixin_gen< "TestMetaprograms\\basic.fsx" >
 // This is RECURSIVE! The DSL metaprogram also references
 // the mixin provider and in turn uses the basic metaprogram 
 // to determine one of its values! - head explode!
-type DSL_Test = mixin_gen< "TestMetaprograms\\DSL.fsx" >
+// type DSL_Test = mixin_gen< "TestMetaprograms\\DSL.fsx" >
 
 
 // generates a x = 25  (5 + 20!)
