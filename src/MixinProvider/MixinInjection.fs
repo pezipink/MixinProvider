ed7a93113e9bed6ec62753f3a076853614f5072f
pezi <pezi_pink@pinksquirrellabs.com>
2015-04-12 21:53:41 +0100

    fixes

--- src/MixinProvider/MixinInjection.fs
+++ src/MixinProvider/MixinInjection.fs
@@ -101,35 +101,36 @@ type MixinInjectionProvider() =
              "mixin_inject4Attribute"
              "mixin_inject5Attribute"
              "mixin_inject_endAttribute"]
-       
-        let fsi,prefix = getEvalSession env
-        results.GetAllUsesOfAllSymbols() |> Async.RunSynchronously
-        |> Seq.filter(fun s -> List.exists ((=) s.Symbol.DisplayName) injectionNames)
-        |> Seq.groupBy(fun s -> s.FileName)
-        |> Seq.map(fun (fn,refs) -> fn, refs |> Seq.sortBy(fun s -> s.RangeAlternate.StartLine) |> Seq.toList)
-        |> Seq.map(fun (fn,refs) -> fn, refs |> List.partition(fun s -> s.Symbol.DisplayName.Contains("_end") = false))
-        |> Seq.map(fun (fn,refs) -> fn, refs ||> List.zip)
-        |> Seq.map(fun (fn,refs) -> 
-            let lines = System.IO.File.ReadAllLines fn
-            let lastIndex, newLines =
-                refs        
-                |> List.fold(fun (lastIndex,acc) (start, finish) ->
-                    let expr = 
-                        if prefix <> "" then
-                             sprintf "%s.%s %s" prefix ((lines.[start.RangeAlternate.StartLine-1].Substring(lines.[start.RangeAlternate.StartLine-1].IndexOf("=")+1)).Trim()) (sprintf "@\"%s\"" projectFile)
-                        else
-                             sprintf "%s %s" (lines.[start.RangeAlternate.StartLine-1].Substring(lines.[start.RangeAlternate.StartLine-1].IndexOf("=")+1)) (sprintf "@\"%s\"" projectFile)
-                    let program = 
-                        match fsi.EvalExpression(expr) with 
-                        | Some x -> 
-                            let source = x.ReflectionValue :?> string
-                            source.Split([|Environment.NewLine|], StringSplitOptions.None) |> Array.toList
-                        | _ -> []            
-                    let x = (lines.[lastIndex..start.RangeAlternate.StartLine-1] |> Array.toList) @ program
-                    (finish.RangeAlternate.StartLine,x))(0,[])
-            fn, newLines @ (lines.[lastIndex-1..] |> Array.toList))
-        |> Seq.iter(fun (fn,lines) -> System.IO.File.WriteAllLines(fn,lines))
-        
+        try
+            lock checker (fun _ -> 
+                let fsi,prefix = getEvalSession env
+                results.GetAllUsesOfAllSymbols() |> Async.RunSynchronously
+                |> Seq.filter(fun s -> List.exists ((=) s.Symbol.DisplayName) injectionNames)
+                |> Seq.groupBy(fun s -> s.FileName)
+                |> Seq.map(fun (fn,refs) -> fn, refs |> Seq.sortBy(fun s -> s.RangeAlternate.StartLine) |> Seq.toList)
+                |> Seq.map(fun (fn,refs) -> fn, refs |> List.partition(fun s -> s.Symbol.DisplayName.Contains("_end") = false))
+                |> Seq.map(fun (fn,refs) -> fn, refs ||> List.zip)
+                |> Seq.map(fun (fn,refs) -> 
+                    let lines = System.IO.File.ReadAllLines fn
+                    let lastIndex, newLines =
+                        refs        
+                        |> List.fold(fun (lastIndex,acc) (start, finish) ->
+                            let expr = 
+                                if prefix <> "" then
+                                     sprintf "%s.%s %s" prefix ((lines.[start.RangeAlternate.StartLine-1].Substring(lines.[start.RangeAlternate.StartLine-1].IndexOf("=")+1)).Trim()) (sprintf "@\"%s\"" projectFile)
+                                else
+                                     sprintf "%s %s" (lines.[start.RangeAlternate.StartLine-1].Substring(lines.[start.RangeAlternate.StartLine-1].IndexOf("=")+1)) (sprintf "@\"%s\"" projectFile)
+                            let program = 
+                                match fsi.EvalExpression(expr) with 
+                                | Some x -> 
+                                    let source = x.ReflectionValue :?> string
+                                    source.Split([|Environment.NewLine|], StringSplitOptions.None) |> Array.toList
+                                | _ -> []            
+                            let x = (lines.[lastIndex..start.RangeAlternate.StartLine-1] |> Array.toList) @ program
+                            (finish.RangeAlternate.StartLine,x))(0,[])
+                    fn, newLines @ (lines.[lastIndex-1..] |> Array.toList))
+                |> Seq.iter(fun (fn,lines) -> System.IO.File.WriteAllLines(fn,lines)))
+        with _ -> ()
         projectsCompiling.TryRemove(projectFile) |> ignore
     
     let createEnv seekMode seekLocation staticParams =
@@ -146,7 +147,7 @@ type MixinInjectionProvider() =
         match projectsCompiling.TryAdd(projFile,projOptions) with
         | true -> 
             async{
-                checker.ClearLanguageServiceRootCachesAndCollectAndFinalizeAllTransients()
+                //checker.ClearLanguageServiceRootCachesAndCollectAndFinalizeAllTransients()
                 let! results = checker.ParseAndCheckProject projOptions
                 processResults projFile results env
             } |> Async.Start
@@ -222,13 +223,15 @@ type MixinInjectionProvider() =
         // we need this as ther check async to avoid deadlocks
         // but we don't want the compile to finish before
         // the remaining work has completed 
-        //while projectsCompiling.Count > 0 do
-        //    System.Threading.Thread.Sleep 100
+//        while projectsCompiling.Count > 0 do
+//            System.Threading.Thread.Sleep 100
+
         resultType
 
     override this.PreEvaluate env =         
-        let real = File.Exists env.additionalData.dummyDll
-        if env.staticParams.compileMode = CompileMode.CompileWhenMissisng && real then false else
+//        let real = File.Exists env.additionalData.dummyDll
+//        if env.staticParams.compileMode = CompileMode.CompileWhenMissisng && real then false else
+//        true
         true
     
     override this.Evaluate env =
